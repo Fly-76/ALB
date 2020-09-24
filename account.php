7b0dca96b2cb140348e9f54d75e8b3b9ba95c143
Franck LEUYER <f_leuyer@hotmail.com>
2020-09-24 10:37:00 +0200

    add link to index.php and create account page

---
+++ account.php
@@ -0,0 +1,17 @@
+<?php
+  include "page/header.php";
+  require_once "data/accounts.php";
+  $accounts = get_accounts();
+?>
+
+  <!-- main -->
+  <main class="container">
+    <section class="row">
+
+    </section>
+  </main>
+  <!-- end main -->
+
+<?php
+  include "page/footer.php";
+?>
