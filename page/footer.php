8af984d35af2a7701cf79538777852b6d7fdd2da
Franck LEUYER <f_leuyer@hotmail.com>
2020-09-24 10:24:12 +0200

    footer.php add code to load xxxx.js depending of page

--- page/footer.php
+++ page/footer.php
@@ -17,7 +17,17 @@
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
-  <script src="js/main.js"></script>
+<?php
+  $page = $_SERVER['SCRIPT_NAME'];
+  
+  // extract filename without extension
+  $pos = strrpos($page, "/");
+  if ($pos !== false) { 
+    $page = substr($page, $pos+1);
+    $page = substr($page, 0, -4);
+  }
+?>
+  <script src="js/<?php echo $page ?>.js"></script>
 
 </body>
 </html>
