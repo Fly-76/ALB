78011c31ab9888282a527273b8562cc2b0f48c24
Franck LEUYER <f_leuyer@hotmail.com>
2020-10-07 17:06:36 +0200

    end of day

--- page/database.php
+++ page/database.php
@@ -4,15 +4,6 @@ function dbConnect()
     try
     {
         $db = new PDO('mysql:host=localhost; dbname=banque_php; charset=utf8', 'root', '');
-
-        // $db = new PDO(
-        //     'mysql:host=franckpadaisback.mysql.db; dbname=franckpadaisback; charset=utf8mb4', 
-        //     'franckpadaisback', 
-        //     '4d410vl4cEbcK'
-        // );
-    
-    
-
         return $db;
     }
     catch(Exception $e)
